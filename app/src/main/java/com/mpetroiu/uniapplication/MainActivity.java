33bce8f5b9117cb3409bdcbb72e657e45e045119
shikaione <mr.petroiu.marius@gmail.com>
2018-03-29 00:38:43 +0300

    first commit

---
+++ app/src/main/java/com/mpetroiu/uniapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mpetroiu.uniapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("dis is tag", "onCreate: on ccreate created");
+    }
+
+}
