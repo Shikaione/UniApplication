0d4f161194563850c3cb9b87bf97075ed4f4764a
Darknorth <mr.petroiu.marius@gmail.com>
2018-04-09 17:48:45 +0300

    New Opening

---
+++ app/src/main/java/com/mpetroiu/uniapplication/OpeningScreen.java
@@ -0,0 +1,13 @@
+package com.mpetroiu.uniapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+    }
+}
