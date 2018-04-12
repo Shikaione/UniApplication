f3b4359d725cfa0b295b0947dd22c68bb342eeb4
Darknorth <mr.petroiu.marius@gmail.com>
2018-04-12 16:59:19 +0300

    Work Update 12/04/2018

--- app/src/main/java/com/mpetroiu/uniapplication/LoginActivity.java
+++ app/src/main/java/com/mpetroiu/uniapplication/LoginActivity.java
@@ -3,11 +3,11 @@ package com.mpetroiu.uniapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OpeningScreen extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opening);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/mpetroiu/uniapplication/WelcomeActivity.java
@@ -0,0 +1,44 @@
+package com.mpetroiu.uniapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button browseBtn;
+    private Button loginbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+
+        final Context context = this;
+
+        browseBtn = findViewById(R.id.browseBtn);
+        loginbtn = findViewById(R.id.loginBtn);
+
+        browseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
