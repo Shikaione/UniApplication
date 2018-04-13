62474a5a7dcf7690c273f1d3db927184900c1997
Darknorth <mr.petroiu.marius@gmail.com>
2018-04-13 16:00:01 +0300

    Work Update 13/04/2018

--- app/src/main/java/com/mpetroiu/uniapplication/MainActivity.java
+++ app/src/main/java/com/mpetroiu/uniapplication/MainActivity.java
@@ -1,33 +1,79 @@
 package com.mpetroiu.uniapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean logged = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         
-         Toolbar menu = (Toolbar) findViewById(R.id.main_menu);
+
+        //Creating the toolbar( menu ) when creating the activity
+         Toolbar menu = (Toolbar) findViewById(R.id.menu);
          setSupportActionBar(menu);
+
+         //No title
          getSupportActionBar().setTitle(null);
+         //Activating the back icon on the menu
          getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
-    public boolean onOptionItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-                default:
-                    return super.onOptionsItemSelected(item);
+    /**
+     * Menu Item
+     * Search Option
+     */
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*Daca Userul este loggat si fiseaza un Menu
+         *Daca nu este logat afiseaza un Guest Menu
+         */
+        if(logged == false) {
+            getMenuInflater().inflate(R.menu.guest_menu, menu);
+        }else{
+            getMenuInflater().inflate(R.menu.user_menu, menu);
         }
+        //Search
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Iteme din Menu si actiunile acestora
+     *
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        else if (id == R.id.action_login) {
+            Intent login = new Intent(this, LoginActivity.class);
+            startActivity(login);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
