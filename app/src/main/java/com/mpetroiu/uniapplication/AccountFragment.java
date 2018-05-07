93f048c0e65b80fe04fc0035b59e17fb70ca9862
Darknorth <mr.petroiu.marius@gmail.com>
2018-05-08 01:30:56 +0300

    Home Work 08.05.2018 : 01:30

--- app/src/main/java/com/mpetroiu/uniapplication/AccountFragment.java
+++ app/src/main/java/com/mpetroiu/uniapplication/AccountFragment.java
@@ -1,19 +1,66 @@
 package com.mpetroiu.uniapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class AccountFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AccountFragment extends Fragment implements View.OnClickListener{
+
+    private final static String TAG = " AccountFragment";
+
+    private FirebaseUser mFirebaseUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        view.findViewById(R.id.deleteAccount).setOnClickListener(this);
+
+        return view;
     }
 
+    private void deleteAccount(){
+
+       mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+       if(mFirebaseUser != null){
+
+            mFirebaseUser.delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "isSuccessfull");
+                                Toast.makeText(getContext(), "Account Deleted!",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getActivity(), LoginOptionsActivity.class));
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.deleteAccount) {
+            deleteAccount();
+        }
+    }
 }
--- app/src/main/java/com/mpetroiu/uniapplication/PasswordFragment.java
+++ app/src/main/java/com/mpetroiu/uniapplication/PasswordFragment.java
@@ -1,19 +1,107 @@
 package com.mpetroiu.uniapplication;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class PasswordFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class PasswordFragment extends Fragment implements View.OnClickListener {
+
+    private final static String TAG = "PasswordFragment";
+
+    private FirebaseUser mFirebaseUser;
+
+    private EditText newPassword, verifiedPassword;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_password, container, false);
+
+        newPassword = (EditText) view.findViewById(R.id.changePassword);
+        verifiedPassword = (EditText) view.findViewById(R.id.verifyChangePassword);
+        view.findViewById(R.id.change).setOnClickListener(this);
+
+        return view;
     }
 
+    private void updatePassword(String newPassword){
+
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (!validateForm()) {
+            return;
+        }
+
+            if(mFirebaseUser != null){
+                    Log.e(TAG, "User : " +mFirebaseUser);
+
+                mFirebaseUser.updatePassword(newPassword).addOnCompleteListener(getActivity(),
+                        new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+
+                            Log.d(TAG, "isSuccessfull");
+                            Toast.makeText(getContext(), "Password updated!",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(getActivity(), LoginOptionsActivity.class));
+                        } else {
+                            Log.d(TAG, "isNotSuccessfull");
+                            Toast.makeText(getContext(), "Failed to update password!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String newPass = newPassword.getText().toString();
+        String verifiedPass = verifiedPassword.getText().toString();
+
+        if (TextUtils.isEmpty(newPass)) {
+            newPassword.setError("Required.");
+            valid = false;
+        } else {
+            newPassword.setError(null);
+        }
+
+        if (TextUtils.isEmpty(verifiedPass)) {
+            verifiedPassword.setError("Required.");
+            valid = false;
+        } else {
+            verifiedPassword.setError(null);
+        }
+
+        if(!newPass.equals(verifiedPass)){
+            Toast.makeText(getActivity(),"Passwords do not match", Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+        return valid;
+    }
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.change) {
+            updatePassword(newPassword.getText().toString());
+        }
+    }
 }
